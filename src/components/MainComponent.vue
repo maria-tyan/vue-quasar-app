cea90f6012e374f9ed82dee5102b1fe353376d19
Maria Efremova <maria375797@gmail.com>
2020-11-30 21:12:05 +0200

    Get data using axios request

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1,5 +1,30 @@
 <template>
   <div>
-    Hello!
+    <div class="" v-for="store in stores" :key="store.code">
+      {{ store.name }}
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      stores: []
+    }
+  },
+  async created () {
+    await this.fetchData()
+  },
+  methods: {
+    async fetchData () {
+      try {
+        const data = await this.$axios.get('https://demoapi.thedenstore.com/api/service?Request=Stores&Language=en-us')
+        this.stores = data.data
+      } catch (error) {
+        console.error(error)
+      }
+    }
+  }
+}
+</script>
