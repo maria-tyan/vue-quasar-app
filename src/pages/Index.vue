7c53c583492adf7ef84b3fcc1e09f02eca7fcfac
Maria Efremova <maria375797@gmail.com>
2020-11-30 20:47:21 +0200

    Quasar CLI instalation

---
+++ src/pages/Index.vue
@@ -0,0 +1,14 @@
+<template>
+  <q-page class="flex flex-center">
+    <MainComponent />
+  </q-page>
+</template>
+
+<script>
+import MainComponent from 'components/MainComponent.vue'
+
+export default {
+  name: 'PageIndex',
+  components: { MainComponent }
+}
+</script>
